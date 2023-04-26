1ac8b65f13335f2e409b8fe84632038594cd3886
Nate Schultz <nate.schultz@outlook.com>
2023-04-26 12:15:34 -0400

    misc

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 import uuid
 import random
+import asyncio
+import logging
+import sys
 
 from ipfsclient.ipfs import Ipfs
 from bizlogic.loan.writer import LoanWriter
@@ -17,24 +20,38 @@ from threads.withdraw_application import WithdrawApplication
 
 ipfsclient = Ipfs()
 num_users = 100
+min_wallet_size = 0
+max_wallet_size = 1000000
+execution_duration = 10
+max_ticks = 100
+
+log = logging.getLogger(__name__)
+logging.basicConfig(filename=f'logs_{__name__}.txt',
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.DEBUG)
 
 # Create user id's with mock nano wallets
 users = [uuid.uuid4() for _ in range(num_users)]
-wallets = {user: random.randint(0, 1000000) for user in users}
+wallets = {user: random.randint(min_wallet_size, max_wallet_size) for user in users}
 
 threads = [
-    AcceptLoanOffer(1, 'accept-loan'),
-    CreateApplication(2, 'create-application'),
-    CreateLoanOffer(3, 'create-loan-offer'),
-    CreatePayment(4, 'create-payment'),
-    CreateVouch(5, 'create-vouch'),
-    WalletActivity(6, 'wallet-activity'),
-    WithdrawApplication(7, 'withdraw-application')
+    AcceptLoanOffer(1, 'accept-loan', log, max_ticks),
+    CreateApplication(2, 'create-application', log, max_ticks),
+    CreateLoanOffer(3, 'create-loan-offer', log, max_ticks),
+    CreatePayment(4, 'create-payment', log, max_ticks),
+    CreateVouch(5, 'create-vouch', log, max_ticks),
+    WalletActivity(6, 'wallet-activity', log, max_ticks),
+    WithdrawApplication(7, 'withdraw-application', log, max_ticks)
 ]
 
-# Start Threads
-for thread in threads:
-    thread.run()
 
-# Clean up: delete everything
-# TODO
+async def main():
+    """Execute threads."""
+    tasks = [asyncio.create_task(thread.run()) for thread in threads]
+    asyncio.gather(*tasks)
+
+asyncio.gather(main())
+sys.stdout.flush()
+sys.stderr.flush()
--- src/threads/create_vouch.py
+++ src/threads/create_vouch.py
@@ -1,20 +1,47 @@
-import threading
 from typing import Self
+import logging
+
+log = logging.getLogger(__name__)
+logging.basicConfig(filename=f'logs_{__name__}.txt',
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.DEBUG)
 
 
-class CreateVouch(threading.Thread):
+
+class CreateVouch():
     """Thread for randomly creating vouches."""
 
     thread_id: int
     name: str
+    log: logging.Logger
+    max_ticks: int
+    tick_count: int
 
-    def __init__(self: Self, thread_id: int, name: str) -> None:
+    def __init__(
+            self: Self,
+            thread_id: int,
+            name: str,
+            log: logging.Logger,
+            max_ticks: int) -> None:
         """Construct."""
-        threading.Thread.__init__(self)
         self.thread_id = thread_id
         self.name = name
+        self.log = log
+        self.max_ticks = max_ticks
+        self.tick_count = 0
 
-    def run(self: Self) -> None:
+    async def run(self: Self) -> None:
         """Start thread."""
-        print("Starting " + self.name)
-        print("Exiting " + self.name)
+        log.debug("Starting " + self.name)
+        while self.tick_count < self.max_ticks:
+            self.tick_count += 1
+            log.debug("Running " + self.name)
+            await self.tick()
+
+        log.debug("Exiting " + self.name)
+
+    async def tick(self: Self) -> None:
+        """Iterate."""
+        pass
